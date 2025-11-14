510c40bcd5788ce0127da5f2d017aa21a829549c
Petru Zdroba <petru.zdroba@gmail.com>
2025-11-14 11:26:34 +0200

    sectio 1

--- dev_springboot/01-spring-boot-demo/src/main/java/com/pz/springboot/demo/mycoolapp/rest/FunRestController.java
+++ dev_springboot/01-spring-boot-demo/src/main/java/com/pz/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.pz.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,20 @@ public class FunRestController {
     @GetMapping("/fortune")
     public String getFortune(){
         return "Today is your lucky day.";
+    }
+
+    //inject properties from application.properties
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    //endpoint that uses both
+
+    @GetMapping("/teaminfo")
+    private String teamInfo(){
+        return String.format("Coach: %s, Team: %s", coachName, teamName);
     }
 }
